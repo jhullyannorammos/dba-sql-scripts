44790053ba48fd6f44c28464e4cec3e1645f9583
Juliano Ramos <jhullyannoorammos@gmail.com>
2022-09-02 11:08:37 -0300

    tentando resolver commit...

---
+++ 5-Valores_iniciais.sql
@@ -0,0 +1,8 @@
+/*******************
+***INSERT Iniciais **
+********************/
+/* 
+ Importe os valores do arquivo Modelo_de_dados.xlsx
+*/
+-- Administrador padrão
+INSERT INTO Usuario(EMAIL,SENHA,TIPO_USUARIO) VALUES('admin@admin.com','123456778',100);
